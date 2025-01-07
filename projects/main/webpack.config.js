bcebb7c64e900994d5aef5be204657f1c128acfc
yusufansari <yusufansari563@gmail.com>
2025-01-07 12:40:54 +0000

    fixing production issue

--- projects/main/webpack.config.js
+++ projects/main/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./main.ts",
+  entry: "/projects/main/main.ts",
   module: {
     rules: [
       {
